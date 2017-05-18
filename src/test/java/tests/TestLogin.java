d7f8d9f8f46da4ef61a3adf5689e20b27168608b
gyenesada <gyenesada@caesar.elte.hu>
2017-05-18 17:36:17 +0200

    Gyenes A. - próba teszt. -

---
+++ src/test/java/tests/TestLogin.java
@@ -0,0 +1,77 @@
+package tests;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class TestLogin {
+ //Maven can't execute this tests yet..
+    
+/*  private WebDriver driver;
+  private String baseUrl;
+  private boolean acceptNextAlert = true;
+  private StringBuffer verificationErrors = new StringBuffer();
+
+  @Before
+  public void setUp() throws Exception {
+    driver = new FirefoxDriver();
+    baseUrl = "http://localhost:8080/";
+    driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+  }
+
+  @Test
+  public void testLogin() throws Exception {
+    driver.get(baseUrl + "/Project_Tools/");
+    driver.findElement(By.linkText("Bejelentkezés")).click();
+    driver.findElement(By.id("password_input")).clear();
+    driver.findElement(By.id("password_input")).sendKeys("test");
+    driver.findElement(By.id("username_input")).clear();
+    driver.findElement(By.id("username_input")).sendKeys("test");
+    driver.findElement(By.cssSelector("button.btn.btn-primary")).click();
+  }
+
+  @After
+  public void tearDown() throws Exception {
+    driver.quit();
+    String verificationErrorString = verificationErrors.toString();
+    if (!"".equals(verificationErrorString)) {
+      fail(verificationErrorString);
+    }
+  }
+
+  private boolean isElementPresent(By by) {
+    try {
+      driver.findElement(by);
+      return true;
+    } catch (NoSuchElementException e) {
+      return false;
+    }
+  }
+
+  private boolean isAlertPresent() {
+    try {
+      driver.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  private String closeAlertAndGetItsText() {
+    try {
+      Alert alert = driver.switchTo().alert();
+      String alertText = alert.getText();
+      if (acceptNextAlert) {
+        alert.accept();
+      } else {
+        alert.dismiss();
+      }
+      return alertText;
+    } finally {
+      acceptNextAlert = true;
+    }
+  }*/
+}
+
